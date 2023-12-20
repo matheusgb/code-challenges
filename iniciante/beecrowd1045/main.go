1ebae7b2c1ae368135373e5edd1ff8ddbfb728f0
matheusgb <matheusgbr98@gmail.com>
2023-12-19 21:47:51 -0300

    1045

--- iniciante/beecrowd1045/main.go
+++ iniciante/beecrowd1045/main.go
@@ -5,11 +5,35 @@ import (
 )
 
 func main() {
-	var A, B int
-	fmt.Scan(&A, &B)
-	if A%B == 0 || B%A == 0 {
-		fmt.Printf("Sao Multiplos\n")
+	var A, B, C float64
+	fmt.Scan(&A, &B, &C)
+
+	if A < B {
+		B, A = A, B
+	}
+	if A < C {
+		A, C = C, A
+	}
+	if B < C {
+		C, B = B, C
+	}
+
+	if A >= B+C {
+		fmt.Println("NAO FORMA TRIANGULO")
 	} else {
-		fmt.Printf("Nao sao Multiplos\n")
+		if A*A == B*B+C*C {
+			fmt.Println("TRIANGULO RETANGULO")
+		}
+		if A*A > B*B+C*C {
+			fmt.Println("TRIANGULO OBTUSANGULO")
+		}
+		if A*A < B*B+C*C {
+			fmt.Println("TRIANGULO ACUTANGULO")
+		}
+		if A == B && B == C {
+			fmt.Println("TRIANGULO EQUILATERO")
+		} else if A == B || B == C || A == C {
+			fmt.Println("TRIANGULO ISOSCELES")
+		}
 	}
 }
