89b99cf86cbfcd2b756ccdb77d8d8a50245b1c78
matheusgb <matheusgbr98@gmail.com>
2023-12-31 15:52:22 -0300

    leetcode 347

---
+++ leetcode/347/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+// há solução melhor com o bucketsort https://leetcode.com/problems/top-k-frequent-elements/solutions/81602/java-o-n-solution-bucket-sort/
+func topKFrequent(nums []int, k int) []int {
+	m := map[int]int{}
+	for _, n := range nums {
+		m[n]++
+	}
+
+	mr := map[int][]int{}
+	for n, count := range m {
+		mr[count] = append(mr[count], n)
+	}
+
+	o := []int{}
+	for i := len(nums); len(o) != k; i-- {
+		for _, n := range mr[i] {
+			if len(o) != k {
+				o = append(o, n)
+			}
+		}
+	}
+
+	return o
+}
+
+func main() {
+	fmt.Println(topKFrequent([]int{3, 2, 1, 2}, 1))
+}
